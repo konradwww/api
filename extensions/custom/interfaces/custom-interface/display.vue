57cac0156d1258b7262c8825b2e90f9b2f0b0cb8
konrad wocko <konrad.wocko.extern@sport1.de>
2018-11-30 11:54:05 +0100

    Added POC custom inteface.

---
+++ extensions/custom/interfaces/custom-interface/display.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="no-wrap">{{ displayValue }}</div>
+</template>
+
+<script>
+  import mixin from "@directus/mixins";
+
+
+  export default {
+    mixins: [mixin],
+
+    created: function() {
+         console.log('created interface-example');
+    },
+
+
+    computed: {
+      displayValue() {
+        let value = this.value;
+
+        if (this.options.formatValue) {
+          value = this.$helpers.formatTitle(value);
+        }
+
+        return value;
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .test {
+    width: 100%;
+  }
+</style>
---
+++ extensions/custom/interfaces/custom-interface/input.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    {{message}}
+  <input
+    type="text"
+    :id="name"
+    :value="value"
+    :readonly="readonly"
+    @input="$emit('input', $event.target.value)"
+  />
+  </div>
+</template>
+
+<script>
+  import mixin from "@directus/mixins";
+
+  export default {
+    mixins: [mixin],
+    methods: {
+      updateValue(rawValue) {
+        if (!rawValue) return;
+
+        let value = rawValue;
+
+        if (this.options.trim) {
+          if (!this.value || value.length > this.value.length) {
+            value = value.trim();
+          }
+        }
+
+        this.$emit("input", value);
+      }
+    },
+    data: function() {
+      return {
+        message: "this is custom interface input2",
+        message2: "test2"
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  input {
+    width: 100%;
+    max-width: var(--width-medium);
+    height:  var(--input-height);
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+</style>
